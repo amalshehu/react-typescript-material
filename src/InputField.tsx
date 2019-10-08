43e8eeb4f22be34561fb4afabd1d0a33c38a9f2b
Amal Shehu <amalshehu@ashmxmz-titan.local>
2019-10-08 19:20:23 +0530

    Add styles to input box

--- src/InputField.tsx
+++ src/InputField.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 import { FieldProps } from 'formik';
 import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
 interface Props extends FieldProps {
   placeholder: string;
 }
-
+const useStyles = makeStyles({
+  inputBox: {
+    minWidth: 350,
+    'margin-bottom': '20px',
+    'margin-top': '10px'
+  }
+});
 export const InputField: React.FC<Props> = ({ placeholder, field }) => {
-  return <TextField placeholder={placeholder} {...field}></TextField>;
+  const classes = useStyles();
+  return (
+    <TextField
+      className={classes.inputBox}
+      placeholder={placeholder}
+      {...field}
+    ></TextField>
+  );
 };
